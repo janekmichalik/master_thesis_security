6fc141af52286d3080c886c10bb05b69f21c269e
Jan Michalik <jan.michalik@intel.com>
2022-08-19 15:03:15 +0200

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,1 +1,11 @@
-# lab.py
+import subprocess
+import os
+
+def main():
+  student_password = input("Haslo studenta: ")
+  if len(student_password) < 8:
+    raise Exception(f"Haslo studenta za krotkie: {student_password}")
+  return subprocess.call(f"echo {student_password}", shell=True)
+
+if __name__ == "__main__":
+  main()
